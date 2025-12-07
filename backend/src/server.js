554d460247694b30b70518b6d4dcf820ea809541
Eklavya Chauhan <lavpc0312@gmail.com>
2025-12-07 14:19:45 +0530

    commit 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,16 +12,26 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 
 //connectDB();
 
 //middleware - just before you send the response, you can do something
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+} else {
+    // Optional: For production, you can allow all or set your specific domain
+    app.use(cors()); 
+}
+
 app.use(express.json()); //helps us to fetch browser's. we use use() function to add middleware
-app.use(
-    cors({
-        origin:"http://localhost:5173",
-    })
-)
+
 app.use(rateLimiter);
 
 //our simple custom middleware
@@ -31,11 +42,16 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 connectDB().then(() => {
-    app.listen(PORT, ()=>{
+    app.listen(PORT, () => {
         console.log("Server started on PORT:", PORT);
     });
 })
-
-
-//mongodb+srv://lavpc0312_db_user:01WaochLCJl6hDUB@cluster0.gyyirfs.mongodb.net/?appName=Cluster0
