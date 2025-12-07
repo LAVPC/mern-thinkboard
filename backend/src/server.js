7eb58b6a6318bbb2958b0cc48c2aa8f1fd462a0c
Eklavya Chauhan <lavpc0312@gmail.com>
2025-12-07 14:32:35 +0530

    third commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
 import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-//const express = require("express");
 
 dotenv.config();
 
@@ -14,44 +13,35 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-
-//connectDB();
-
-//middleware - just before you send the response, you can do something
-
+// middleware
 if (process.env.NODE_ENV !== "production") {
-    app.use(
-        cors({
-            origin: "http://localhost:5173",
-        })
-    );
-} else {
-    // Optional: For production, you can allow all or set your specific domain
-    app.use(cors()); 
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-
-app.use(express.json()); //helps us to fetch browser's. we use use() function to add middleware
-
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-//our simple custom middleware
-// app.use((req,resizeBy,next)=>{
-//     console.log(`Request method is ${req.method"} & Request URL is ${req.url}`);
-//     next();
-// })
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Server started on PORT:", PORT);
-    });
-})
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
